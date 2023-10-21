dd9ce890e25b38515127d052e56e7305f08121cf
root <root@DESKTOP-I1S954I.localdomain>
2023-10-21 10:26:15 +0530

    demo.java has been commited and completed

--- demo.java
+++ demo.java
@@ -2,6 +2,10 @@ public class Demo{
 	public static void main (String args[]){
 		//hello world program
 		System.out.Println("Hello World");
+	} 
+
+	public void sayhello(){
+	System.out.Println("say hello");
 	}
 }
 
